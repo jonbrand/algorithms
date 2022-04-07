bddae06af07f3be69b0546aaa70642df6e49ebd2
jonbrand <jonatas@grupoeureka.com.br>
2022-04-07 17:20:52 -0300

    fix: binary search algorithm

--- binary-search/main.js
+++ binary-search/main.js
@@ -15,10 +15,10 @@ function binary_search(list, item) {
     }
   }
 
-  return null;
+  return false;
 }
 
-const my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const my_list = [11, 25, 34, 55, 101, 180, 190, 255, 500];
 
-console.log(binary_search(my_list, 7));
+console.log(binary_search(my_list,101));
 console.log(binary_search(my_list, -1));
